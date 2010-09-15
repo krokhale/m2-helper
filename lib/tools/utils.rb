63f1ded8bbd65eb29a4516fa61f12da069bf4672
Krishna Rokhale <krishna.rokhale@gmail.com>
2010-09-15 17:31:44 -0400

    minor changes

--- lib/tools/utils.rb
+++ lib/tools/utils.rb
@@ -2,20 +2,44 @@ module M2Helper
     
 class Utils
    
-   def self.theme_updater(permalinks)
+   def self.themes_link(permalinks)
      
      links = permalinks.split(",")
-     updated_theme = Store::Base.find_by_permalink(links[0]).themes[0]
-     links.delete_at(0)
-     
+      updated_theme = Store::Base.find_by_permalink(links[0]).themes[0]
+      links.delete_at(0)
 
-     links.each do |link|
-       Store::Base.find_by_permalink(link).themes << updated_theme
-     end
-     
-     puts "Done!"
+
+      links.each do |link|
+        if !(Store::Base.find_by_permalink(link).nil?)
+        Store::Base.find_by_permalink(link).themes << updated_theme
+        @result = true
+      else
+        @result = false
+      end
+      end
+      
+      return @result
         
    end
    
-end
+   
+   def self.themes_clone(permalinks)
+     
+       links = permalinks.split(",")
+       updated_theme = Store::Base.find_by_permalink(links[0]).themes[0].clone
+       updated_theme.save!
+       links.delete_at(0)  
+
+       links.each do |link|
+          if !(Store::Base.find_by_permalink(link).nil?)
+          Store::Base.find_by_permalink(link).themes << updated_theme
+          @result = true
+        else
+          @return = false
+        end
+        end
+        
+        return @result
+   end
+ end
  end
